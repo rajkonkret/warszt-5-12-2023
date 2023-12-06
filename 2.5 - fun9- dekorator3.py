60a3070f80b1ec6d87f2e3773e41f6e74914a75d
rjan <rajkonkret660@gmail.com>
2023-12-06 11:59:46 +0100

    2 - update fun9 dekorator3

--- 2.5 - fun9- dekorator3.py
+++ 2.5 - fun9- dekorator3.py
@@ -22,9 +22,35 @@ def my_function():
     total = sum(range(15000000))
     print("Sum is:", total)
 
-# def my_function_np():
+
+@measure_time
+def my_function_sum():
+    suma = 0
+    for i in range(15000000):
+        suma += i
+    print("Sum is:", suma)
 
 
-# my_function()
+@measure_time
+def my_function_np():
+    total = np.sum(np.arange(15000000), dtype=np.int64)
+    print("Sum is:", total)
+
+
+my_function()
 # Czas wykonania funkcji my_function: 0.9335784912109375 sekundy
 # 11:20
+my_function_np()
+
+# Sum is: 112499992500000
+# Czas wykonania funkcji my_function: 0.5210573673248291 sekundy
+# Sum is: 112499992500000
+# Czas wykonania funkcji my_function_np: 0.044116973876953125 sekundy
+my_function_sum()
+
+# Sum is: 112499992500000
+# Czas wykonania funkcji my_function: 0.532174825668335 sekundy
+# Sum is: 112499992500000
+# Czas wykonania funkcji my_function_np: 0.0420536994934082 sekundy
+# Sum is: 112499992500000
+# Czas wykonania funkcji my_function_sum: 0.6128120422363281 sekundy
