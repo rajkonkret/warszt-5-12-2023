10b256c77aaa40db5068893626065cfc45575731
rjan <rajkonkret660@gmail.com>
2023-12-07 11:10:42 +0100

    3 - add kl8

---
+++ 3.5 - zadanka-klasy-funkcje.py
@@ -0,0 +1,11 @@
+# stworzenie ksiaki telefonicznej z wykorzystaniem petli while
+# dodaj kontakt, usun kontakt, wyszukaj, wyswietl lisste kontaktow
+
+
+# stworzyc system zarzadzanai biblioteka
+# dodaj ksiazke, usun ksiazke, wypozycz ksiazke, zwroc ksiazke
+# uzyc klass np.: Book, Library
+# obsłuzyc błedu
+# mozna dodac klase user - bedzie pamietał jakie ksiazki wypozyczył
+# 11:20
+
