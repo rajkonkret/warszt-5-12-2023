904bfde45beb62bb69ebcf6730044738eaf25092
rjan <rajkonkret660@gmail.com>
2023-12-07 15:31:32 +0100

    3 - update kl17

---
+++ 3.16 - kl17 mixin.py
@@ -0,0 +1,25 @@
+# klasy mixin
+# wykorzystuja wielodziedziczenie
+# uzywaja klas, których nie warto tworzyc instancji
+import json
+
+
+class JsonMixin:
+    import json  # wprzypadku klas mixin dopuszcczone
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+class XmlMixin:
+    def to_xml(self):
+        pass
+
+
+class Contact(JsonMixin, XmlMixin):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+contact = Contact("Jan", "Kowalski")
+print(contact.to_json())  # {"first_name": "Jan", "last_name": "Kowalski"}
