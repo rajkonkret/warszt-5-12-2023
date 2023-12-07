7440ceabc9fedefe50f07a39507f47402fde8469
rjan <rajkonkret660@gmail.com>
2023-12-07 15:16:13 +0100

    3 - update kl15

---
+++ 3.14 - kl14-iterator.py
@@ -0,0 +1,28 @@
+class Count:
+    def __init__(self, low, high):
+        self.current = low
+        self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.high:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
+
+
+counter = Count(1, 5)
+while True:
+    try:
+        number = next(counter)
+        print(number)
+    except StopIteration:
+        break
+
+counter2 = Count(1, 10)
+print(next(counter2))
+print(next(counter2))
+print(next(counter2))
