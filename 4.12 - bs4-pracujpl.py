3f7ac577a846e8cb162afd57b9e1e675f4267167
rjan <rajkonkret660@gmail.com>
2023-12-08 15:01:06 +0100

    4 - add selenium pracujpl

---
+++ 4.12 - bs4-pracujpl.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+url = 'https://www.pracuj.pl/praca/programista%20python;kw'
+# url = 'https://justjoin.it'
+driver = webdriver.Chrome()
+driver.get(url)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+# print(soup)
+
+data = soup.find('script', {'id': '__NEXT_DATA__'}).text
+# print(data)
+offers_count = int(data.split('offersCount":')[1].split(",")[0])
+print(offers_count)  # 592
